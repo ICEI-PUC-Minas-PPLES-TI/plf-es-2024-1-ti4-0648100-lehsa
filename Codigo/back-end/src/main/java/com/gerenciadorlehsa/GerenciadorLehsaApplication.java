5114f32f46244ca2642cb2bbf320ac3d9f586d0a
Lucas Soares <polonio230@hotmail.com>
2024-03-13 23:10:43 -0300

    build: started configuring Spring Boot

---
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/GerenciadorLehsaApplication.java
@@ -0,0 +1,14 @@
+package com.gerenciadorlehsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLehsaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GerenciadorLehsaApplication.class, args);
+	}
+
+}
