d731eae59be2ca07caa96471976a0cde00580a3f
lucasht02 <lucashemetrio@gmail.com>
2024-03-18 19:40:00 -0300

    menu fix

--- Codigo/front-end/app/admin/NavBar.tsx
+++ Codigo/front-end/app/admin/NavBar.tsx
@@ -13,7 +13,9 @@ const NavBar = () => {
             <Image src='/logo.jpg' width={150} height={0} alt='logo' className='m-auto'></Image>
             <div className='flex flex-col gap-2 pl-5'>
                 <Link href='/admin' className={`pl-6 py-2 rounded-l-full ${pathname === '/admin' ? 'bg-primary text-white' : ''}`}>Dashboard</Link>
-                <Link href='/admin/equipamentos' className={`pl-6 py-2 rounded-l-full ${pathname === '/admin/equipamentos' ? 'bg-primary text-white' : ''}`}>Equipamentos</Link>
+                <Link href='/admin/itens' className={`pl-6 py-2 rounded-l-full ${pathname === '/admin/itens' ? 'bg-primary text-white' : ''}`}>Itens</Link>
+                <Link href='/admin/agendamentos' className={`pl-6 py-2 rounded-l-full ${pathname === '/admin/agendamentos' ? 'bg-primary text-white' : ''}`}>Agendamentos</Link>
+                <Link href='/admin/usuarios' className={`pl-6 py-2 rounded-l-full ${pathname === '/admin/usuarios' ? 'bg-primary text-white' : ''}`}>Usuários</Link>
             </div>
         </aside>
     )
