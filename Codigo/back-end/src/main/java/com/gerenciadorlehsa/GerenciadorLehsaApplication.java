51d0d5a9de3dcf3fed48a6efcf91ad1fc9bb7e7c
Lucas Soares <polonio230@hotmail.com>
2024-03-23 18:42:01 -0300

    refactor: up line

--- Codigo/back-end/src/main/java/com/gerenciadorlehsa/GerenciadorLehsaApplication.java
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/GerenciadorLehsaApplication.java
@@ -1,13 +1,9 @@
 package com.gerenciadorlehsa;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GerenciadorLehsaApplication{
-
-
 
 	public static void main(String[] args) {
 
