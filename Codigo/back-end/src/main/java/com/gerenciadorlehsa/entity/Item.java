f677fe7f4028d744778dccffd3469330de75a1fe
Lucca <lovfaria@sga.pucminas.br>
2024-03-28 18:06:32 -0300

    Mudança inicial Entity Item

--- Codigo/back-end/src/main/java/com/gerenciadorlehsa/entity/Item.java
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/entity/Item.java
@@ -3,7 +3,12 @@ package com.gerenciadorlehsa.entity;
 
 import com.gerenciadorlehsa.entity.enums.TipoItem;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import static com.gerenciadorlehsa.util.ConstantesErroValidadorUtil.MSG_ERRO_SENHA;
 
 @Entity
 @Table(name = Item.NOME_TABELA)
@@ -18,16 +23,25 @@ public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID", unique = true, nullable = false, updatable = false)
     private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo_de_item", nullable = false)
     private TipoItem tipoItem;
 
+    @Column(name = "quantidade", nullable = false)
+    @Min(0)
+    private int quantidade;
 
+    @Column(name = "valor_unitario", nullable = false)
+    @DecimalMin(value = "0.0", inclusive = false)
+    private float valorUnitario;
 
+    @Column(name = "nome", length = 64, nullable = false)
+    @Size(min = 3, max = 64, message = "Mensagem ainda a definir")
+    private String nome;
 
-
-
-
+    @Column(name = "emprestavel", nullable = false)
+    private boolean emprestavel;
 }
