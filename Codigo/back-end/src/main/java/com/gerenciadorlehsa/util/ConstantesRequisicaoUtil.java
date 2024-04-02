a303824bf8daf921a3d568bf7a1b6b4054f6adc3
Lucas Soares <polonio230@hotmail.com>
2024-04-01 23:06:01 -0300

    refactor: fix CRUD methods

--- Codigo/back-end/src/main/java/com/gerenciadorlehsa/util/ConstantesRequisicaoUtil.java
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/util/ConstantesRequisicaoUtil.java
@@ -41,7 +41,7 @@ public class ConstantesRequisicaoUtil {
             "\n\"hora\": \"%s\"" + "," +
             "\n}";
 
-    public static final String[] PROPRIEDADES_IGNORADAS = new String[]{"id", "senha", "perfilUsuario", "endereco", "dataCriacao", "dataUltimaModificacao"};
+    public static final String[] PROPRIEDADES_IGNORADAS = new String[]{"id", "password", "perfilUsuario"};
 
     public static final List<String> CHAVES_ENDERECO_CONTROLLER = new ArrayList<>(asList("status", "mensagem", "id_endereco"));
 
