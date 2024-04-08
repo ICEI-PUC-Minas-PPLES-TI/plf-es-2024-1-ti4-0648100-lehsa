016a7b7abe9cfb4d5bb85e36b924efc3eb939ef9
duda-amaral <dudaamaralmap3110@gmail.com>
2024-04-08 16:23:06 -0300

    fix: select de tipo e select emprestavel #30

--- Codigo/front-end/app/admin/itens/editar/[id]/page.tsx
+++ Codigo/front-end/app/admin/itens/editar/[id]/page.tsx
@@ -2,10 +2,9 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Switch } from '@/components/ui/switch'
-import React, {useEffect, useState} from 'react'
-import { useRouter} from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation';
 
 function getIdFromUrl(url: string) {
     const segments = url.split('/');
@@ -16,6 +15,8 @@ const EditarItem = () => {
 
     const router = useRouter();
     const [item, setItem] = useState(null);
+    const [tipoItem, setTipoItem] = useState("");
+    const [itemEmprestavel, setItemEmprestavel] = useState("");
     const consultaId = getIdFromUrl(window.location.pathname);
 
     useEffect(() => {
@@ -61,8 +62,8 @@ const EditarItem = () => {
             const nome = formData.get('nome')
             const quantidade = formData.get('quantidade')
             const valor_unitario = formData.get('valor_unitario')
-            const emprestavel = formData.get('emprestavel')
-            const tipo_item = formData.get('tipo_item')
+            const emprestavel = itemEmprestavel
+            const tipo_item = tipoItem
 
             const response = await fetch(`http://localhost:8080/item/${id}`, {
                 method: 'PUT',
@@ -70,7 +71,7 @@ const EditarItem = () => {
                     'Authorization': `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c3VhcmlvMDNAZXhhbXBsZS5jb20iLCJleHAiOjE3MTI3MjgxOTN9.42cBdN7Fnd81t8oroFGRAyAbKWjPoWsvSGq5puDR0d5Gbkh2faWUaS09KHe64B-vi3ZhEEdZT_kq-i1QXrlXEA`,
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({nome, quantidade, valor_unitario, emprestavel, tipo_item})
+                body: JSON.stringify({ nome, quantidade, valor_unitario, emprestavel, tipo_item })
             });
 
             if (!response.ok) {
@@ -87,27 +88,29 @@ const EditarItem = () => {
         <div className='bg-white rounded-xl h-3/4 p-5 ml-60 w-4/12'>
             <h3 className='text-xl font-semibold'>Atualizar</h3>
             <form className="max-w-md" onSubmit={handleSubmit}>
-            <Label htmlFor='nome'>Nome</Label>
-            <Input placeholder='nome do item' name='nome' />
-            <Label htmlFor="quantidade">Quantidade</Label>
-            <Input placeholder='0' name='quantidade' type="number" />
-            <Label htmlFor="valor_unitario">Valor</Label>
-            <Input placeholder='R$ 000,00' name='valor_unitario' type="number"/>
-            <div className='flex flex-col space-y-3'>
-                <Label htmlFor='emprestavel'>Emprestavel?</Label>
-                <Switch name='emprestavel' />
-            </div>
-            <Label htmlFor='tipo_item'>Categoria</Label>
-                <Select>
-                    <SelectTrigger>
-                        <SelectValue placeholder='Tipo'/>
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value='EQUIPAMENTO'>Equipamento</SelectItem>
-                        <SelectItem value='VIDRARIA'>Vidraria</SelectItem>
-                    </SelectContent>
-                </Select>
-            <Button type="submit" className="bg-green-500 hover:bg-green-600 my-20 float-right">Salvar</Button>
+                <Label htmlFor='nome'>Nome</Label>
+                <Input placeholder='nome do item' name='nome' />
+                <Label htmlFor="quantidade">Quantidade</Label>
+                <Input placeholder='0' name='quantidade' type="number" />
+                <Label htmlFor="valor_unitario">Valor</Label>
+                <Input placeholder='R$ 000,00' name='valor_unitario' type="number" />
+                <div>
+                    <Label htmlFor='emprestavel'>Emprestavel?</Label>
+                    <select value={itemEmprestavel} onChange={(e) => setItemEmprestavel(e.target.value)}
+                            className="border border-gray-300 rounded-md p-2 flex">
+                        <option value="">Tipo</option>
+                        <option value="true">Sim</option>
+                        <option value="false">Não</option>
+                    </select>
+                </div>
+                <Label htmlFor='tipo_item'>Categoria</Label>
+                <select value={tipoItem} onChange={(e) => setTipoItem(e.target.value)}
+                        className="border border-gray-300 rounded-md p-2 flex">
+                    <option value="">Tipo</option>
+                    <option value="EQUIPAMENTO">Equipamento</option>
+                    <option value="VIDRARIA">Vidraria</option>
+                </select>
+                <Button type="submit" className="bg-green-500 hover:bg-green-600 my-20 float-right">Salvar</Button>
             </form>
         </div>
     )
