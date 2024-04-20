13a5a8388c604cba85a19ad98ff2ab52d0f77024
Lucas Soares <polonio230@hotmail.com>
2024-04-19 22:50:15 -0300

    build: config classes

--- Codigo/back-end/src/main/java/com/gerenciadorlehsa/entity/User.java
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/entity/User.java
@@ -24,36 +24,13 @@ import static com.gerenciadorlehsa.util.ConstantesErroValidadorUtil.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class User extends RepresentationModel<User> implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "ID", unique = true, nullable = false, updatable = false)
-    private UUID id;
-
-    @Column(name = "NOME", nullable = false)
-    private String nome;
-
-    @Column(name = "EMAIL", unique = true, nullable = false)
-    @Pattern(regexp = "^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.?([a-z]+)?$", message = MSG_ERRO_EMAIL)
-    private String email;
+public class User extends Pessoa {
 
     @Column(name = "PASSWORD", nullable = false)
     @NotBlank(message = "A senha é obrigatória")
     @Size(min = 6, max = 200, message = MSG_ERRO_SENHA)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-
-    @Column(name = "TELEFONE", nullable = false)
-    @Pattern(regexp = "(^[0-9]{2})?(\\s|-)?(9?[0-9]{4})-?([0-9]{4}$)", message = MSG_ERRO_TELEFONE)
-    private String telefone;
-
-    @CPF
-    @Column(name = "CPF", unique = true, nullable = false)
-    @Pattern(regexp = "(^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$)", message = MSG_ERRO_CPF)
-    private String cpf;
 
 
     @Column(name = "PERFIL_USUARIO", nullable = false)
