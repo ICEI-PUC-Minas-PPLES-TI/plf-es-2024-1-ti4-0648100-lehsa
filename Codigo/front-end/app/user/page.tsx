47cce6b357cd5ed3cb7299c66bc9b6e90980b7ed
Vitor Stahlberg <vitorlberg@gmail.com>
2024-04-29 20:48:57 -0300

    Feat: integração adm agendamentos view #48

--- Codigo/front-end/app/user/page.tsx
+++ Codigo/front-end/app/user/page.tsx
@@ -1,14 +1,35 @@
-"use client";
-
+"use client"
+import React, { useEffect, useState } from "react";
+import Cookie from "js-cookie";
+import { Agendamento as AgendamentoType } from "@/components/types";
 import Agendamento from "@/components/Agendamento";
+import TopMenu from "@/components/topMenu";
+import SearchBar from "@/components/SearchBar";
 import FilterSelect from "@/components/FilterSelect";
 import ItensCard from "@/components/ItemCard";
-import SearchBar from "@/components/SearchBar";
-import TopMenu from "@/components/topMenu";
-import React, { useState } from "react";
 
 const UserPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [agendamentos, setAgendamentos] = useState<AgendamentoType[]>([]);
+
+  useEffect(() => {
+    const authToken = Cookie.get("token") ?? "";
+    fetch("http://localhost:8080/agendamento", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log("API response:", data); // Log the data to see its structure
+        setAgendamentos(data);
+      })
+      .catch(error => {
+        console.error("Error fetching agendamentos:", error);
+      });
+  }, []);
 
   return (
     <div className="max-w-7xl m-auto space-y-5">
@@ -17,16 +38,14 @@ const UserPage = () => {
         <div className="w-full bg-white h-auto rounded-2xl p-5">
           <h2 className="font-semibold text-xl mb-4">Seus Agendamentos</h2>
           <div className="grid gap-4 justify-center lg:grid-cols-2">
-            <Agendamento />
-            <Agendamento />
-            <Agendamento />
+            {agendamentos.map(agendamento => (
+              <Agendamento key={agendamento.id} items={agendamento.itens} />
+            ))}
           </div>
         </div>
       </div>
-      <div className="w-full bg-white   rounded-2xl p-5">
-        <h2 className="font-semibold text-xl mb-6">
-          Itens disponíveis para agendamento
-        </h2>
+      <div className="w-full bg-white rounded-2xl p-5">
+        <h2 className="font-semibold text-xl mb-6">Itens disponíveis para agendamento</h2>
         <div className="flex flex-1 justify-between my-5">
           <SearchBar onChange={(e) => setSearchTerm(e.target.value)} />
           <div className="flex justify-items-end space-x-5">
--- Codigo/front-end/components/AgendamentoInfoRow.tsx
+++ Codigo/front-end/components/AgendamentoInfoRow.tsx
@@ -1,102 +1,67 @@
 "use client";
-import Cookie from "js-cookie";
-import React, { useState, useEffect } from "react";
-import { TrashIcon } from "lucide-react";
 
-interface User {
-  id: number;
-  nome: string;
-  email: string;
-  telefone: number;
-  cpf: string;
-  perfil_usuario: number;
+import React from "react";
+import ImageComp from "./ImageComp";
+import { Item, Tecnico } from "./types";
+
+interface AgendamentoInfoRowProps {
+  item: Item;
+  tecnico: Tecnico;
+  dataHoraFim: string;
+  dataHoraInicio: string;
 }
 
-function AgendamentoInfoRow({ searchTerm }: { searchTerm: string }) {
-  const [users, setUsers] = useState<User[]>([]);
-  const [token, setToken] = useState<string>("");
-  const [loggedInUserId, setLoggedInUserId] = useState<number>(0); // State to store logged-in user ID
-
-  useEffect(() => {
-    const authToken = Cookie.get("token") ?? "";
-    setToken(authToken);
-
-    fetch(`http://localhost:8080/usuario`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data: User[]) => {
-        setUsers(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching users:", error);
-      });
-
-    // Decode token to get the ID of the logged-in user
-    try {
-      const decodedToken = JSON.parse(atob(authToken.split(".")[1]));
-      if (decodedToken && decodedToken.userId) {
-        setLoggedInUserId(decodedToken.userId);
-      }
-    } catch (error) {
-      console.error("Error decoding token:", error);
-    }
-  }, []);
-
-  const renderProfileType = (perfil_usuario: number) => {
-    if (perfil_usuario === 1) {
-      return "ADM";
-    } else if (perfil_usuario === 2) {
-      return "Usuário";
-    } else {
-      return "Erro";
-    }
-  };
-
-  // const filteredUsers = users.filter((user) =>
-  //   user.nome.toLowerCase().includes(searchTerm.toLowerCase())
-  // );
-
+const AgendamentoInfoRow: React.FC<AgendamentoInfoRowProps> = ({
+  item,
+  tecnico,
+  dataHoraFim,
+  dataHoraInicio,
+}) => {
   return (
-    <div className="flex justify-center">
-      <div className="table w-[21rem] rounded-xl">
-        <div className="table-header-group">
-          <div className="table-row h-[2rem]">
-            <div className="table-cell text-left pl-5 font-medium">Dia</div>
-            <div className="table-cell text-left pl-5 font-medium">Técnico responsável</div>
-            <div className="table-cell text-left pl-5 font-medium">Contato</div>
-            <div className="table-cell text-left pl-5 font-medium"></div>
+    <div className="mb-5">
+      <div className="w-[42rem] h-[10rem] bg-white rounded-[20px] relative mb-1">
+        <div className="flex justify-between w-full h-[3rem] bg-primary rounded-t-[20px] items-center z-10">
+          <h1 className="ml-[9.5rem] text-white text-lg">{item.nome}</h1>
+        </div>
+        <div className="flex flex-row pt-1 pl-1">
+          <div className="w-[8rem] h-[8rem] bg-gray-200 rounded-[10px] absolute left-[1rem] top-[1rem] z-0"></div>
+          <ImageComp
+            src={`http://localhost:8080/item/img/${item.id}`}
+            alt="item picture"
+            width={400}
+            height={400}
+            className="object-cover w-[8rem] h-[8rem] bg-gray-200 rounded-[10px] absolute left-[1rem] top-[1rem] z-0"
+          />
+        </div>
+        <div className="ml-[10rem] pt-1">
+          {tecnico ? (
+            <>
+              <div className="flex flex-row justify-around gap-1">
+                <div className="flex flex-col">
+                  <p>Técnico: </p>
+                  <p>{tecnico.nome}</p>
+                </div>
+                <div className="flex flex-col">
+                  <p>Email: </p>
+                  <p> {tecnico.email}</p>
+                </div>
+                <div className="flex flex-col">
+                  <p>Telefone: </p>
+                  <p> {tecnico.telefone}</p>
+                </div>
+              </div>
+            </>
+          ) : (
+            <p>Técnico não especificado</p>
+          )}
+          <div className="flex flex-row justify-between pt-6 pr-6">
+            <p className=" text-sm">Início: {dataHoraInicio}</p>
+            <p className=" text-sm">Fim: {dataHoraFim}</p>
           </div>
         </div>
-
-        {/* {filteredUsers.map((user, index) => (
-          <ul
-            className={`usersTable table-row-group ${
-              index % 2 === 0 ? "evenRow" : "oddRow"
-            }`}
-            data-id={user.id}
-            key={user.id}
-          >
-            <li className="table-cell text-left h-[3rem] align-middle pl-5">
-              <p>{user.nome}</p>
-            </li>
-            <li className="table-cell text-left h-[3rem] align-middle pl-5">
-              <p>{user.email}</p>
-            </li>
-            <li className="table-cell text-left h-[3rem] align-middle pl-5">
-              <p>{user.telefone}</p>
-            </li>
-            <li className="table-cell text-left h-[3rem] align-middle pl-5 ">
-            </li>
-          </ul>
-        ))} */}
       </div>
     </div>
   );
-}
+};
 
 export default AgendamentoInfoRow;
