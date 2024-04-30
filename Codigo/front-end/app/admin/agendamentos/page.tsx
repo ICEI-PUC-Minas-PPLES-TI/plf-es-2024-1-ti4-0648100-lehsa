3014cda7aabda4a252ad7ea1acb8073ecf15818a
lucasht02 <lucashemetrio@gmail.com>
2024-04-29 23:44:18 -0300

    Update page.tsx

--- Codigo/front-end/app/admin/agendamentos/page.tsx
+++ Codigo/front-end/app/admin/agendamentos/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Cookie from "js-cookie";
 import { Agendamento as AgendamentoType, Tecnico } from "@/components/types"; // Assuming Tecnico type is imported here
 import Agendamento from "@/components/Agendamento";
+import Link from "next/link";
 
 const Agendamentos = () => {
   const [agendamentos, setAgendamentos] = useState<AgendamentoType[]>([]);
@@ -30,7 +31,7 @@ const Agendamentos = () => {
   return (
     <div>
       {agendamentos.map((agendamento) => (
-        <Agendamento key={agendamento.id} items={agendamento.itens} tecnico={agendamento.tecnico} dataHoraFim={agendamento.dataHoraFim} dataHoraInicio={agendamento.dataHoraInicio}/>
+        <Link href={`/admin/agendamentos/${agendamento.id}`}><Agendamento key={agendamento.id} items={agendamento.itens} tecnico={agendamento.tecnico} dataHoraFim={agendamento.dataHoraFim} dataHoraInicio={agendamento.dataHoraInicio} /></Link>
       ))}
     </div>
   );
