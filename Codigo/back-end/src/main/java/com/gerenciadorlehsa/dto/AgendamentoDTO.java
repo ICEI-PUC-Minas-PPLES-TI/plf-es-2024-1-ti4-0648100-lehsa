f050cf9bca338b612ecc13f33d2fac1cc22c734d
Lucas Soares <polonio230@hotmail.com>
2024-05-12 00:17:56 -0300

    refactor: add jsonIgnore at map

--- Codigo/back-end/src/main/java/com/gerenciadorlehsa/dto/AgendamentoDTO.java
+++ Codigo/back-end/src/main/java/com/gerenciadorlehsa/dto/AgendamentoDTO.java
@@ -1,11 +1,8 @@
 package com.gerenciadorlehsa.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.*;
 
 @Builder
