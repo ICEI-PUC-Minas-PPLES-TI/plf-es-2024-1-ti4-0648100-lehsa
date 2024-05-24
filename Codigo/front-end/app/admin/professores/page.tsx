52a7cd0327a1b7750f43a2c516c019f8a5b33f84
duda-amaral <dudaamaralmap3110@gmail.com>
2024-05-24 17:46:31 -0300

    feat: adicionando página de visualizar professores

--- Codigo/front-end/app/admin/professores/page.tsx
+++ Codigo/front-end/app/admin/professores/page.tsx
@@ -1,10 +1,17 @@
+import TeacherRow from "@/components/TeacherRow";
 import TopMenu from "@/components/topMenu";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Professores = () => {
   
     return (
       <div className="">
         <TopMenu title="Professores" />
+        <div className='mt-10'>
+        <Link href='/admin/emprestimo/cadastro'><Button>+ Novo Professor</Button></Link>
+        </div>
+        <TeacherRow/>
       </div>
     );
   };
